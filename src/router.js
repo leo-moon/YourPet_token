093f941979c98f6f0e3a46ad719f06904cc3a484
Dranyk <dvdranyk@gmail.com>
2023-05-11 22:01:34 +0300

    update

---
+++ src/router.js
@@ -0,0 +1,14 @@
+// export const ROUTES = {
+//     HOME: '/',
+//     NEWS: '/news',
+//     FRIENDS: '/friends',
+//     REGISTER: '/register',
+//     LOGIN: '/login',
+//     USER: '/user',
+//     NOTICES: '/notices',
+//     NOTICES_SELL: '/notices/sell',
+//     NOTICES_FOR_FREE: '/notices/for-free',
+//     NOTICES_LOST_FOUND: '/notices/lost-found',
+//     NOTICES_FAVORITE: '/notices/favorite',
+//     NOTICES_OWN: '/notices/own',
+//   };
