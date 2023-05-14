1e92872a98983db025c82defe32be02c5b9c9786
Dranyk <dvdranyk@gmail.com>
2023-05-14 12:44:38 +0300

    Update AuthNav.jsx

--- src/components/Nav/AuthNav/AuthNav.jsx
+++ src/components/Nav/AuthNav/AuthNav.jsx
@@ -1,14 +1,14 @@
 import css from './AuthNav.module.css';
 // import { Box } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 const AuthNav = () => {
     return (
       <>
-        {/* <div>
+        <div>
           <a className={css.button} href='/login'>Log IN</a>
           <a className={css.button} href='/register'>Registration</a>
-        </div> */}
+        </div>
   
         {/* <NavLink to="/"></NavLink>
         <div>
