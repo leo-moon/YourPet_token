091d8adb5f6d36ff3984cde9386729ca92975b48
Dranyk <dvdranyk@gmail.com>
2023-05-17 13:13:09 +0300

    routs

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 import logo from "../../images/logo.svg"
 
 const Logo = () => {
-  return  <NavLink to="/YourPet">
+  return  <NavLink to="/">
     <img src={logo} alt="YOUR PET" />
   </NavLink>;
 };
