7a3ecde83fc688cf31f14f3c3c6b3019d1344bd4
Dranyk <dvdranyk@gmail.com>
2023-05-18 16:48:57 +0300

    update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,18 @@
 import css from './Nav.module.scss';
+//import { useSelector, useDispatch } from 'react-redux';
 import AuthNav from './AuthNav/AuthNav';
-// import UserNav from './UserNav/UserNav';
+//import UserNav from './UserNav/UserNav';
 import { NavLink } from 'react-router-dom';
-import menuHam from "images/menu-hamburger.svg"
+import menuHam from 'images/menu-hamburger.svg';
+//import { setMenuActive } from 'redux/menuSlice';
 
 const Nav = () => {
+  //const isActive = useSelector(state => state.menu.menuActive);
+  //const isLogin = useSelector(state => state.auth.isLogin);
+  //const dispatch = useDispatch();
+
   return (
     <div className={css.div}>
-     
       <div className={css.menu}>
         <NavLink className={css.item} to="/news">
           News
@@ -20,8 +25,47 @@ const Nav = () => {
         </NavLink>
       </div>
 
+      {/* {isActive === true ? (
+        <NavLink
+          to={'news'}
+          className={css.linkMain}
+          onClick={() => dispatch(setMenuActive(!isActive))}
+        >
+          News
+        </NavLink>
+      ) : (
+        <NavLink to={'news'} className={css.linkMain}>
+          News
+        </NavLink>
+      )}
+      {isActive === true ? (
+        <NavLink
+          to={'notices/sell'}
+          className={css.linkMain}
+          onClick={() => dispatch(setMenuActive(!isActive))}
+        >
+          Find pet
+        </NavLink>
+      ) : (
+        <NavLink to={'notices/sell'} className={css.linkMain}>
+          Find pet
+        </NavLink>
+      )}
+      {isActive === true ? (
+        <NavLink
+          to={'friends'}
+          className={css.linkMain}
+          onClick={() => dispatch(setMenuActive(!isActive))}
+        >
+          Our friends
+        </NavLink>
+      ) : (
+        <NavLink to={'friends'} className={css.linkMain}>
+          Our friends
+        </NavLink>
+      )} */}
       <AuthNav />
-      {/* <UserNav /> */}
+      {/* {isLogin ? <UserNav /> : <AuthNav />} */}
       <NavLink>
         <img className={css.menuHam} src={menuHam} alt="menuHamburger" />
       </NavLink>
