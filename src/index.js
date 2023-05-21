b3c5abe1d2bc3cd1310271d4d36c8e3eaa7bb7fe
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-05-21 09:38:46 +0300

    for push

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AuthLayout>
-          {/* <BrowserRouter basename="/YourPet"> */}
-          <BrowserRouter >
+          <BrowserRouter basename="/YourPet">
             <App />
           </BrowserRouter>
         </AuthLayout>
