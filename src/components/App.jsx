11739beb445dbb955b3ce92f12c346da46575c74
Dranyk <dvdranyk@gmail.com>
2023-05-21 10:01:51 +0300

    fix-conflict

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,5 @@
-
-import { React, Suspense } from 'react';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import NoticesPage from "./../pages/NoticesPage/NoticesPage.jsx";
-
-// import FavoritePage from "./../pages/FavoritePage/FavoritePage";
-// import MyOwnPage from "./../pages/MyOwnPage/MyOwnPage";
-
 export const App = () => {
   return (
-    <>
-<BrowserRouter basename="YourPet/">
-<Suspense>
-      <Routes> 
-          <Route path="/notices/:category" element={<NoticesPage />}/>
-      </Routes>
-</Suspense>
-  </BrowserRouter>
-    </>
-  );
-};
-
-          
-
-
-          
-
-
-    
-
+    <div>Hello</div>
+    );
+  };
