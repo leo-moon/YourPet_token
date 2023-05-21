db5f0ab0455a57c287377ba3cbd2e9368f86dfe1
Dranyk <dvdranyk@gmail.com>
2023-05-21 15:37:33 +0300

    update

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -4,7 +4,6 @@ import SvgInsert from '...';
 // тут треба посилання на собачі іконки
 
 import { useSelector } from 'react-redux';
-import Loader from 'components/Loader/Loader';
 
 const ModalNotice = ({
   id,
