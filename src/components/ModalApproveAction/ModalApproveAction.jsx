1f5e64b761a79573006213094cc294d79ff89c15
Dranyk <dvdranyk@gmail.com>
2023-05-22 11:59:52 +0300

    Update ModalApproveAction.jsx

--- src/components/ModalApproveAction/ModalApproveAction.jsx
+++ src/components/ModalApproveAction/ModalApproveAction.jsx
@@ -4,7 +4,7 @@ import scss from './modal-approve-action.module.scss'
 import PropTypes from 'prop-types';
 
 export const ModalApproveAction = ({ onClose }) => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   const dispatch = useDispatch();
 
