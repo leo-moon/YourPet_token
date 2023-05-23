f8b7e67f4e2aca0091738781bacfc6a2df7d2e80
Marat Baikin <3371335@gmail.com>
2023-05-23 10:34:14 +0300

    Fetch user data

--- src/pages/UserPage/UserPageCard/UserPageCard.jsx
+++ src/pages/UserPage/UserPageCard/UserPageCard.jsx
@@ -1,6 +1,27 @@
+import UserFormData from '../UserFormData/UserFormData';
+import Logout from '../Logout/Logout';
+
 import css from './UserPageCard.module.css';
 
-const UserPageCard = ({ children }) => {
-  return <div className={css.UserPageCard}>{children}</div>;
+import { CameraIcon, AddPhotoIcon } from 'images/icons/userPageIcons';
+
+const UserPageCard = () => {
+  return (
+    <div className={css.UserPageCard}>
+      <div>
+        <div className={css.photo}>
+          <AddPhotoIcon color={'#54ADFF'} />
+        </div>
+        <div className={css.editPhotoWrapper}>
+          <CameraIcon color={'#54ADFF'} />
+          <span className={css.editPhotoTitle}>Edit photo</span>
+        </div>
+      </div>
+      <div className={css.formWrapper}>
+        <UserFormData />
+        <Logout />
+      </div>
+    </div>
+  );
 };
 export default UserPageCard;
