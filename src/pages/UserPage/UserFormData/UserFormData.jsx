d8a4a3cd2e98f8aaf4931711b3f959364dce11ff
Marat Baikin <3371335@gmail.com>
2023-05-24 17:22:34 +0300

    Load avatar and send to backand

--- src/pages/UserPage/UserFormData/UserFormData.jsx
+++ src/pages/UserPage/UserFormData/UserFormData.jsx
@@ -2,20 +2,16 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useEffect, useState } from 'react';
 
 import instance from 'redux/auth/auth';
+import Logout from '../Logout/Logout';
 
 import css from './UserFormData.module.css';
 
-import { EditIcon } from 'images/icons/userPageIcons';
-
-// import {
-//   AddPetIcon,
-//   LogoutIcon,
-//   CameraIcon,
-//   CheckIcon,
-//   EditIcon,
-//   AddPhotoIcon,
-//   PetLegIcon,
-// } from 'images/icons/userPageIcons';
+import {
+  EditIcon,
+  CameraIcon,
+  CloseIcon,
+  ConfirmIcon,
+} from 'images/icons/userPageIcons';
 
 const initialValues = {
   name: '',
@@ -23,10 +19,12 @@ const initialValues = {
   birthday: '',
   phone: '',
   city: '',
+  image: null,
 };
 
 const UserFormData = () => {
   const [userData, setUserData] = useState([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const fetchData = async () => {
     try {
@@ -41,148 +39,157 @@ const UserFormData = () => {
     fetchData();
   }, []);
 
+  const handleFileChange = (event, setFieldValue) => {
+    const file = event.target.files[0];
+    setFieldValue('image', file);
+    setIsSubmitting(false);
+  };
+
+  const handleFormSubmit = async values => {
+    try {
+      setIsSubmitting(true);
+      const formData = new FormData();
+      formData.append('image', values.image);
+
+      await instance.patch('/api/auth/users/updateavatar', formData);
+    } catch (error) {
+      console.error('Error updating avatar:', error);
+    }
+    // finally {
+    //   setIsSubmitting(false);
+    // }
+  };
+
+  const handleDeleteClick = setFieldValue => {
+    setFieldValue('image', null);
+  };
+
   const DataUser = userData.map(
-    ({ name, email, Birthday, Phone, City }, index) => (
-      <Formik
-        key={index}
-        initialValues={initialValues}
-        // validationSchema={userSchema}
-        // onSubmit={handleSubmit}
-      >
-        <Form autoComplete="off" className={css.form}>
-          <label className={css.fieldWrapper}>
-            <span className={css.fieldName}>Name:</span>
-            <Field
-              className={css.field}
-              type="text"
-              placeholder="Enter name"
-              name="name"
-              value={name}
-            />
-            <EditIcon color={'#54ADFF'} className={css.editIcon} />
-            <ErrorMessage name="name" component="div" />
-          </label>
-          <label className={css.fieldWrapper}>
-            <span className={css.fieldName}>Email:</span>
-            <Field
-              className={css.field}
-              type="text"
-              placeholder="Enter email"
-              name="email"
-              value={email}
-            />
-            <EditIcon color={'#54ADFF'} className={css.editIcon} />
-            <ErrorMessage name="email" component="div" />
-          </label>
-          <label className={css.fieldWrapper}>
-            <span className={css.fieldName}>Birthday:</span>
-            <Field
-              className={css.field}
-              type="text"
-              placeholder="Enter birthday"
-              name="birthday"
-              value={Birthday}
-            />
-            <EditIcon color={'#54ADFF'} className={css.editIcon} />
-            <ErrorMessage name="birthday" component="div" />
-          </label>
-          <label className={css.fieldWrapper}>
-            <span className={css.fieldName}>Phone:</span>
-            <Field
-              className={css.field}
-              type="text"
-              placeholder="Enter phone"
-              name="phone"
-              value={Phone}
-            />
-            <EditIcon color={'#54ADFF'} className={css.editIcon} />
-            <ErrorMessage name="phone" component="div" />
-          </label>
-          <label className={css.fieldWrapper}>
-            <span className={css.fieldName}>City:</span>
-            <Field
-              className={css.field}
-              type="text"
-              placeholder="Enter city"
-              name="city"
-              value={City}
-            />
-            <EditIcon color={'#54ADFF'} className={css.editIcon} />
-            <ErrorMessage name="city" component="div" />
-          </label>
-        </Form>
-      </Formik>
+    ({ name, email, Birthday, Phone, City, avatarURL }, index) => (
+      <div key={index}>
+        <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
+          {formikProps => (
+            <Form autoComplete="off" className={css.form}>
+              <div>
+                <div className={css.photo}>
+                  {(formikProps.values.image && (
+                    <img
+                      src={URL.createObjectURL(formikProps.values.image)}
+                      alt="Preview"
+                    />
+                  )) || <img src={avatarURL} alt="name" />}
+                </div>
+
+                {(formikProps.values.image && (
+                  <div className={css.editPhotoWrapper}>
+                    <button
+                      type="submit"
+                      className={`${css.submitBtn} ${
+                        isSubmitting ? css.submitting : ''
+                      }`}
+                      disabled={isSubmitting}
+                    >
+                      <ConfirmIcon
+                        color={isSubmitting ? '#00C3AD' : '#54ADFF'}
+                      />
+                    </button>
+
+                    <span className={css.editPhotoTitle}>Confirm</span>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        handleDeleteClick(formikProps.setFieldValue)
+                      }
+                    >
+                      <CloseIcon color={'#ec1f18'} width={13} height={13} />
+                    </button>
+                  </div>
+                )) || (
+                  <label className={css.editPhotoWrapper}>
+                    <input
+                      className={css.uploadbtn}
+                      type="file"
+                      accept="image/*"
+                      onChange={event =>
+                        handleFileChange(event, formikProps.setFieldValue)
+                      }
+                    />
+                    <CameraIcon color={'#54ADFF'} />
+                    <span className={css.editPhotoTitle}>Edit photo</span>
+                  </label>
+                )}
+              </div>
+              <div className={css.formFieldWrapper}>
+                <label className={css.fieldWrapper}>
+                  <span className={css.fieldName}>Name:</span>
+                  <Field
+                    className={css.field}
+                    type="text"
+                    placeholder="Enter name"
+                    name="name"
+                    value={name}
+                  />
+                  <EditIcon color={'#54ADFF'} className={css.editIcon} />
+                  <ErrorMessage name="name" component="div" />
+                </label>
+                <label className={css.fieldWrapper}>
+                  <span className={css.fieldName}>Email:</span>
+                  <Field
+                    className={css.field}
+                    type="text"
+                    placeholder="Enter email"
+                    name="email"
+                    value={email}
+                  />
+                  <EditIcon color={'#54ADFF'} className={css.editIcon} />
+                  <ErrorMessage name="email" component="div" />
+                </label>
+                <label className={css.fieldWrapper}>
+                  <span className={css.fieldName}>Birthday:</span>
+                  <Field
+                    className={css.field}
+                    type="text"
+                    placeholder="Enter birthday"
+                    name="birthday"
+                    value={Birthday}
+                  />
+                  <EditIcon color={'#54ADFF'} className={css.editIcon} />
+                  <ErrorMessage name="birthday" component="div" />
+                </label>
+                <label className={css.fieldWrapper}>
+                  <span className={css.fieldName}>Phone:</span>
+                  <Field
+                    className={css.field}
+                    type="text"
+                    placeholder="Enter phone"
+                    name="phone"
+                    value={Phone}
+                  />
+                  <EditIcon color={'#54ADFF'} className={css.editIcon} />
+                  <ErrorMessage name="phone" component="div" />
+                </label>
+                <label className={css.fieldWrapper}>
+                  <span className={css.fieldName}>City:</span>
+                  <Field
+                    className={css.field}
+                    type="text"
+                    placeholder="Enter city"
+                    name="city"
+                    value={City}
+                  />
+                  <EditIcon color={'#54ADFF'} className={css.editIcon} />
+                  <ErrorMessage name="city" component="div" />
+                </label>
+              </div>
+            </Form>
+          )}
+        </Formik>
+        <Logout />
+      </div>
     )
   );
 
   return <>{DataUser}</>;
-  // <Formik
-  //   initialValues={initialValues}
-  //   // validationSchema={userSchema}
-  //   // onSubmit={handleSubmit}
-  // >
-  //   <Form autoComplete="off" className={css.form}>
-  //     <label className={css.fieldWrapper}>
-  //       <span className={css.fieldName}>Name:</span>
-  //       <Field
-  //         className={css.field}
-  //         type="text"
-  //         placeholder="Enter login"
-  //         name="name"
-  //         // value={login}
-  //       />
-  //       <EditIcon color={'#54ADFF'} className={css.editIcon} />
-  //       <ErrorMessage name="name" component="div" />
-  //     </label>
-  //     <label className={css.fieldWrapper}>
-  //       <span className={css.fieldName}>Email:</span>
-  //       <Field
-  //         className={css.field}
-  //         type="text"
-  //         placeholder="Enter email"
-  //         name="email"
-  //         // value={email}
-  //       />
-  //       <EditIcon color={'#54ADFF'} className={css.editIcon} />
-  //       <ErrorMessage name="email" component="div" />
-  //     </label>
-  //     <label className={css.fieldWrapper}>
-  //       <span className={css.fieldName}>Birthday:</span>
-  //       <Field
-  //         className={css.field}
-  //         type="text"
-  //         placeholder="Enter birthday"
-  //         name="birthday"
-  //         // value={password}
-  //       />
-  //       <EditIcon color={'#54ADFF'} className={css.editIcon} />
-  //       <ErrorMessage name="birthday" component="div" />
-  //     </label>
-  //     <label className={css.fieldWrapper}>
-  //       <span className={css.fieldName}>Phone:</span>
-  //       <Field
-  //         className={css.field}
-  //         type="text"
-  //         placeholder="Enter phone"
-  //         name="phone"
-  //         // value={password}
-  //       />
-  //       <EditIcon color={'#54ADFF'} className={css.editIcon} />
-  //       <ErrorMessage name="phone" component="div" />
-  //     </label>
-  //     <label className={css.fieldWrapper}>
-  //       <span className={css.fieldName}>City:</span>
-  //       <Field
-  //         className={css.field}
-  //         type="text"
-  //         placeholder="Enter city"
-  //         name="city"
-  //         // value={password}
-  //       />
-  //       <EditIcon color={'#54ADFF'} className={css.editIcon} />
-  //       <ErrorMessage name="city" component="div" />
-  //     </label>
-  //   </Form>
-  // </Formik>
 };
+
 export default UserFormData;
--- src/pages/UserPage/UserPageCard/UserPageCard.jsx
+++ src/pages/UserPage/UserPageCard/UserPageCard.jsx
@@ -1,26 +1,11 @@
 import UserFormData from '../UserFormData/UserFormData';
-import Logout from '../Logout/Logout';
 
 import css from './UserPageCard.module.css';
-
-import { CameraIcon, AddPhotoIcon } from 'images/icons/userPageIcons';
 
 const UserPageCard = () => {
   return (
     <div className={css.UserPageCard}>
-      <div>
-        <div className={css.photo}>
-          <AddPhotoIcon color={'#54ADFF'} />
-        </div>
-        <div className={css.editPhotoWrapper}>
-          <CameraIcon color={'#54ADFF'} />
-          <span className={css.editPhotoTitle}>Edit photo</span>
-        </div>
-      </div>
-      <div className={css.formWrapper}>
-        <UserFormData />
-        <Logout />
-      </div>
+      <UserFormData />
     </div>
   );
 };
