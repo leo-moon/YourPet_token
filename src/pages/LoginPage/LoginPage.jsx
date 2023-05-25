80928f93d332d3fdd6d8f8f9fdcacf97fc2ddc03
Oksana Kyryliuk <coksanna@gmail.com>
2023-05-25 14:30:51 +0300

    add Loader to LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
 
 import { LoginForm } from '../../components/LoginForm/LoginForm';
 import Container from 'components/Container/Container';
 import { login } from '../../redux/auth/auth-operations';
-import { isUserLogin } from '../../redux/auth/auth-selectors';
+import {
+  isUserLogin,
+  selectError,
+  selectLoading,
+} from '../../redux/auth/auth-selectors';
 
 const LoginPage = () => {
+  const error = useSelector(selectError);
+  const loading = useSelector(selectLoading);
   const isLogin = useSelector(isUserLogin);
   const dispatch = useDispatch();
 
@@ -16,6 +23,10 @@ const LoginPage = () => {
 
   if (isLogin) {
     return <Navigate to="/user" />;
+  }
+
+  if (loading && !error) {
+    return <Loader />;
   }
   return (
     <Container>
