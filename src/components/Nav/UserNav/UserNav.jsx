303dcbf4417c4c85d85971992820b85f1f5efc40
Dranyk <dvdranyk@gmail.com>
2023-05-25 21:37:01 +0300

    name

--- src/components/Nav/UserNav/UserNav.jsx
+++ src/components/Nav/UserNav/UserNav.jsx
@@ -1,14 +1,31 @@
 import css from "./UserNav.module.scss"
 import { NavLink } from 'react-router-dom';
 import userIcon from 'images/user-1.svg';
+import {selectUser} from 'redux/auth/auth-selectors'
+import { useSelector } from "react-redux";
+
 
 const UserNav = () => {
+  const data = useSelector(selectUser)
+  const userName = data.user.name
+
+  const checkName = () =>{
+    if(userName === ""){
+      const result = "User";
+      return result;
+    }
+    else {
+      const result = userName;
+      return result
+    }
+  };
+  const usName =  checkName();
     return (
       <div className={css.div}>
         <NavLink className={css.user} to="/user">
         <img className={css.icon} src={userIcon} alt="userIcon" /> 
         {/* {name} */}
-        <p className={css.name}>NAME</p> 
+        <p className={css.name}>{usName}</p> 
         </NavLink>
       </div>
     );
