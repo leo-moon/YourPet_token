692d0b12d2dee715511181f83bf867544f43de96
Dranyk <dvdranyk@gmail.com>
2023-05-26 10:26:44 +0300

    add_logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks/useAuth';
 import userIcon from 'images/user-1.svg';
 import pawprint from 'images/pawprint.svg';
+import logo from '../../images/logo.svg';
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
@@ -20,35 +21,43 @@ const Header = () => {
       : (document.body.style.overflow = 'unset');
   };
   const { isLoggedIn } = useAuth();
-  
+
   return (
     <div className={css.headerStyle}>
       <div className={css.header}>
-        <Logo/>
-        
-          <Nav />
-          <div className={css.nav}>
+        <Logo />
+
+        <Nav />
+        <div className={css.nav}>
           {isLoggedIn ? <UserNav /> : <AuthNav />}
           <button
-          className={css.menuIcon}
-          onClick={toggle}
-          aria-label="mobile menu"
-        >
-          {isOpen ? (
-            <img className={css.menuBur} src={closeIcon} alt="menuBurger" />
-          ) : (
-            <img className={css.menuBur} src={menuHam} alt="menuBurger" />
-          )}
-        </button>
+            className={css.menuIcon}
+            onClick={toggle}
+            aria-label="mobile menu"
+          >
+            {isOpen ? (
+              <img className={css.menuBur} src={closeIcon} alt="menuBurger" />
+            ) : (
+              <img className={css.menuBur} src={menuHam} alt="menuBurger" />
+            )}
+          </button>
         </div>
 
-        
         {isOpen && (
           <div className={css.burger}>
             <div className={css.listMenu}>
+              <NavLink to="/">
+                <img className={css.logo} src={logo} alt="YOUR PET" />
+              </NavLink>
+              ;
               {isLoggedIn ? (
                 <div className={css.authNavigate}>
-                  <NavLink  className={css.iconMob} to="/user" onClick={toggle} style={{ width: 164 }}>
+                  <NavLink
+                    className={css.iconMob}
+                    to="/user"
+                    onClick={toggle}
+                    style={{ width: 164 }}
+                  >
                     <img className={css.icon} src={userIcon} alt="userIcon" />
                   </NavLink>
                 </div>
@@ -73,7 +82,11 @@ const Header = () => {
                   </NavLink>
                 </div>
               )}
+              
               <div className={css.navdiv}>
+              <NavLink to="/">
+                <img className={css.logoTab} src={logo} alt="YOUR PET" />
+              </NavLink>
                 <NavLink className={css.link} to="/news" onClick={toggle}>
                   News
                 </NavLink>
